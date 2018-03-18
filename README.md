c42dd8780faecf6132c95b1efd40bc88eb9a74b3
Myklinn <37506462+Myklinn@users.noreply.github.com>
2018-03-18 14:35:33 +0100

    Website upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# projectaclletsel
-Website
---
+++ js/functions.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$(".button-collapse").sideNav();
+});
+
+$(document).ready(function() {
+	var slideIndex = 0;
+	carousel();
+
+	function carousel() {
+		var i;
+		var x = document.getElementsByClassName("myFeiten");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+		slideIndex++;
+		if (slideIndex > x.length) {slideIndex = 1}
+			x[slideIndex-1].style.display = "block";
+		setTimeout(carousel, 5000);
+	}
+});
